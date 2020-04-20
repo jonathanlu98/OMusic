304a61c5b034b1c239d8fe64a4290fdae7fdff81
jonathanlu98 <694442612@qq.com>
2020-04-20 15:28:51 +0800

    Initial Commit

---
+++ OMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OMusic
+//
+//  Created by Jonathan Lu on 2020/4/20.
+//  Copyright © 2020 Jonathan Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
